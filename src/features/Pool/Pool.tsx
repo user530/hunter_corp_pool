163817d0524dbc33b3214ebb0ab62b5ff3de7917
user530 <m.timarsuev@gmail.com>
2024-03-15 11:57:49 +0300

    Add and draw balls

--- src/features/Pool/Pool.tsx
+++ src/features/Pool/Pool.tsx
@@ -1,12 +1,42 @@
-import { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Pool.module.scss';
+import { Ball, drawBall, newBall } from './Ball';
 
 export const Pool = () => {
     const poolRef = useRef<HTMLCanvasElement>(null);
-
+    const [balls, setBalls] = useState<Ball[]>([]);
+    console.log(balls);
     const handleClick = () => console.log('Canvas clicked!');
     const handleMouseMove = () => console.log('Mouse moved!');
-    const handleAddBallClick = () => console.log('Add ball clicked!');
+
+    const handleAddBallClick = () => {
+        // No canvas = no balls
+        if (!poolRef.current) return;
+
+        const { width, height } = poolRef.current;
+
+        // Add new ball
+        setBalls([...balls, newBall(width, height, 30)]);
+    };
+
+    // Visualize each time balls update
+    React.useEffect(
+        () => {
+            console.log('Effect');
+            if (!poolRef.current) return;
+
+            const ctx = poolRef.current.getContext('2d')!;
+            const { width, height, } = poolRef.current;
+            console.log(ctx, width, height);
+            // Clear canvas
+            ctx.clearRect(0, 0, width, height);
+            // Draw balls
+            balls.forEach(
+                ball => drawBall(ctx, ball)
+            )
+        },
+        [balls]
+    )
 
     return (
         <div className={styles['game-wrapper']}>
