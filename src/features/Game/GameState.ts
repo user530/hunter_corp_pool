1ec934f5552634b9fd0408488c95c943c7bc5dd9
user530 <m.timarsuev@gmail.com>
2024-03-16 23:21:58 +0300

    Works fine on elastic bounce, but bugs out with inelastic collision

--- src/features/Game/GameState.ts
+++ src/features/Game/GameState.ts
@@ -5,6 +5,11 @@ interface Drawable {
 }
 
 class Ball implements Drawable {
+    // Density koefficient
+    private readonly _K = 1;
+    private _m: number;
+    private readonly _id = Symbol('id');
+
     constructor(
         private _x: number,
         private _y: number,
@@ -12,7 +17,10 @@ class Ball implements Drawable {
         private _dx: number,
         private _dy: number,
         private _col: BallColors,
-    ) { }
+    ) {
+        // Calculate mass based on size and density
+        this._m = this._K * Math.PI * _r * _r;
+    }
 
     get coords(): [number, number] {
         return [this._x, this._y];
@@ -44,8 +52,17 @@ class Ball implements Drawable {
         this._col = color;
     }
 
+    get mass() {
+        return this._m;
+    }
+
+    get id() {
+        return this._id;
+    }
+
     static randomBall({ fieldWidth, fieldHeight }: { fieldWidth: number, fieldHeight: number }): Ball {
-        const r = Math.min(fieldWidth / 25, fieldHeight / 25);
+        // const r = Math.min(fieldWidth / 25, fieldHeight / 25);
+        const r = 100;
         const x = Math.max(r, Math.min(fieldWidth - r, Math.random() * fieldWidth));
         const y = Math.max(r, Math.min(fieldHeight - r, Math.random() * fieldHeight));
         const dx = Math.random() * 5 * (Math.round(x) % 2 === 0 ? 1 : -1);
@@ -56,8 +73,10 @@ class Ball implements Drawable {
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
+        const [x, y] = this.coords;
+
         ctx.beginPath();
-        ctx.arc(this._x, this._y, this._r, 0, 2 * Math.PI);
+        ctx.arc(x, y, this.radius, 0, 2 * Math.PI);
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.closePath();
@@ -69,11 +88,56 @@ class Ball implements Drawable {
      * @returns True is balls collide, false otherwise
      */
     doCollide(otherBall: Ball): boolean {
-        const deltaX = otherBall._x - this._x;
-        const deltaY = otherBall._y - this._y;
+        // Prevent 'self-collision'
+        if (this.id === otherBall.id) return false;
+
+        // Check 'static' collision (not calculating movement)
+        const deltaX = otherBall.coords[0] - this.coords[0];
+        const deltaY = otherBall.coords[1] - this.coords[1];
+
         const distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
 
-        return distance < this._r + otherBall._r;
+        if (distance < this.radius + otherBall.radius) {
+            console.log('Static Collision!');
+            console.log(this.velocity, otherBall.velocity);
+
+            return true;
+        }
+
+        // Check 'dynamic' collision (prevent 'telefraging')
+        // const [x1, y1] = otherBall.coords;
+        // const [dx1, dy1] = otherBall.velocity;
+
+        // // Relative velocity
+        // const relativeVel: [number, number] = [dx1 - this._dx, dy1 - this._dy];
+
+        // const something = Math.sqrt(relativeVel[0] * relativeVel[0] + relativeVel[1] * relativeVel[1]);
+        return false
+        // return something >= this._r + otherBall._r;
+    }
+
+    handleCollision(otherBall: Ball): void {
+        const [dx0, dy0] = this.velocity;
+        const m0 = this.mass;
+
+        const [dx1, dy1] = otherBall.velocity;
+        const m1 = otherBall.mass;
+
+        // Restitution koefficient
+        const e = 0.98;
+
+        const thisNewVel: [number, number] = [
+            ((m0 - e * m1) * dx0 + (1 + e) * m1 * dx1) / (m0 + m1),
+            ((m0 - e * m1) * dy0 + (1 + e) * m1 * dy1) / (m0 + m1)
+        ];
+
+        const otherNewVel: [number, number] = [
+            ((1 + e) * m0 * dx0 + (m1 - e * m0) * dx1) / (m0 + m1),
+            ((1 + e) * m0 * dy0 + (m1 - e * m0) * dy1) / (m0 + m1)
+        ];
+
+        this.velocity = thisNewVel;
+        otherBall.velocity = otherNewVel;
     }
 }
 
@@ -89,8 +153,16 @@ export class GameState {
         return this._balls;
     }
 
+    get width() {
+        return this._fieldWidth;
+    }
+
+    get height() {
+        return this._fieldHeight;
+    }
+
     addBall(newBall: Ball) {
-        this._balls.push(newBall);
+        this.balls.push(newBall);
     }
 
     /**
@@ -108,8 +180,8 @@ export class GameState {
             // Generate new ball
             let newBall = Ball.randomBall(
                 {
-                    fieldWidth: this._fieldWidth,
-                    fieldHeight: this._fieldHeight
+                    fieldWidth: this.width,
+                    fieldHeight: this.height
                 }
             );
 
@@ -118,18 +190,24 @@ export class GameState {
                 (newFlag, ball) => (newFlag && !ball.doCollide(newBall)), true
             );
 
-            if (ready) this._balls.push(newBall);
+            if (ready) this.addBall(newBall);
         }
     }
 
     updateState(): void {
-        this._balls.forEach(
+        this.balls.forEach(
             ball => {
                 const [x, y] = ball.coords;
                 const [dx, dy] = ball.velocity;
-                const r = ball.radius;
 
                 ball.coords = [x + dx, y + dy];
+
+                // Placeholder, think of some more efficient collision check
+                for (let i = 0; i < this.balls.length; ++i) {
+                    const otherBall = this.balls[i];
+                    const collision = ball.doCollide(otherBall);
+                    if (collision) ball.handleCollision(otherBall);
+                }
 
                 // Border collision applied
                 this.ballToBorder(ball);
@@ -146,8 +224,8 @@ export class GameState {
         let yBounce = false;
 
         // Right border
-        if (x + r > this._fieldWidth) {
-            ball.coords = [this._fieldWidth - r, y];
+        if (x + r > this.width) {
+            ball.coords = [this.width - r, y];
             xBounce = true;
         }
 
@@ -164,8 +242,8 @@ export class GameState {
         }
 
         // Bottom border
-        if (y + r > this._fieldHeight) {
-            ball.coords = [ball.coords[0], this._fieldHeight - r];
+        if (y + r > this.height) {
+            ball.coords = [ball.coords[0], this.height - r];
             yBounce = true;
         }
 
